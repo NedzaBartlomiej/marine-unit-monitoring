bd67452bf9d70aa1aff4661083445563a23de15a
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-03-17 19:06:26 +0100

    trying to setup mongodb repSet (docker-compose probably - ok, mongo-init -> rs init probably - not ok, overall problem/error: not recognizin replica members, something about - local.oplog.rs Coll not found) - all the problems are visible when starting the app (with docker-compose up ofc).

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,3 +1,16 @@
+rs.initiate(
+    {
+        _id: "marine-unit-monitoring-db-rs",
+        version: 1,
+        members: [
+            {_id: 0, host: "mongodb1:27017"},
+            {_id: 1, host: "mongodb2:27017"},
+            {_id: 2, host: "mongodb3:27017"}
+        ]
+    }
+)
+
+
 db = db.getSiblingDB('marine_unit_monitoring');
 
 db.createCollection('ship_track_history');
