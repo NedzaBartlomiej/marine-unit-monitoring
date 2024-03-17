7b39a5be50776c6dbea701f60f96de09bd088eda
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-03-17 22:54:46 +0100

    further development to deploy mongodb replicas (rs.initiate needs to run when mongodb instances are ready - todo)

--- docker-entrypoint-initdb.d/mongo-init.js
+++ docker-entrypoint-initdb.d/mongo-init.js
@@ -1,15 +1,16 @@
-rs.initiate(
-    {
-        _id: "marine-unit-monitoring-db-rs",
-        version: 1,
-        members: [
-            {_id: 0, host: "mongodb1:27017"},
-            {_id: 1, host: "mongodb2:27017"},
-            {_id: 2, host: "mongodb3:27017"}
-        ]
-    }
-)
+// todo zeby zainicjowac rs
+//  trzeba poczekac az wszytskie mongodb[n] sie uruchomia
+//  i wtedy wywolac rs.initiate()
 
+rs.initiate({
+    _id: 'marine-unit-monitoring-db-rs',
+    version: 1,
+    members: [
+        {_id: 0, host: 'mongodb1:27017'},
+        {_id: 1, host: 'mongodb2:27018'},
+        {_id: 2, host: 'mongodb3:27019'}
+    ]
+})
 
 db = db.getSiblingDB('marine_unit_monitoring');
 
