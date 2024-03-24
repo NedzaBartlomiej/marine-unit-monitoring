942152d64382353fe2529c2b879c341a1ee913b0
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-03-24 17:09:05 +0100

    save rs.config fo setting priority for members of mongo rs - to use this when writing readme file in the future

--- docker-entrypoint-init.d/rs-init.sh
+++ docker-entrypoint-init.d/rs-init.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 echo "---- es-init.sh ----"
 
-echo "#### RS.INITIATE() ####"
+echo "#### RS.INITIATE() ####" # todo: needs to set mongodb-primary to primary always!
 mongosh <<BLOCK
 rs.initiate({
     _id: 'marine-unit-monitoring-db-rs',
     version: 1,
     members: [
-        {_id: 0, host: 'mongodb-primary:27017', priority: 2},
-        {_id: 1, host: 'mongodb2:27017', priority: 1},
-        {_id: 2, host: 'mongodb3:27017', priority: 1}
+        {_id: 0, host: 'mongodb-primary:27017'},
+        {_id: 1, host: 'mongodb2:27017'},
+        {_id: 2, host: 'mongodb3:27017'}
     ]
 })
 BLOCK
+
+# add it to readme in installation info
+#cfg = rs.conf()
+#cfg.members[0].priority = 100
+#cfg.members[1].priority = 1
+#cfg.members[2].priority = 1
+#rs.reconfig(cfg)
