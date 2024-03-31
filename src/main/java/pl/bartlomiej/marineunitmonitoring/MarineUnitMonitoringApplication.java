dd3838e5201b2b623343e1c05806e1e6e36097fd
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-03-31 21:32:00 +0200

    add dependency and annotation for non reactive mongo

--- src/main/java/pl/bartlomiej/marineunitmonitoring/MarineUnitMonitoringApplication.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/MarineUnitMonitoringApplication.java
@@ -2,12 +2,14 @@ package pl.bartlomiej.marineunitmonitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableReactiveMongoRepositories
+@EnableMongoRepositories
 public class MarineUnitMonitoringApplication {
 
     public static void main(String[] args) {
