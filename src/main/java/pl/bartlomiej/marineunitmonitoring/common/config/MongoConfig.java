4add69d0ac31a8a1c272c6be87b8e9b0749bd8ef
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-03-31 21:30:18 +0200

    create config for non reactive mongo

--- src/main/java/pl/bartlomiej/marineunitmonitoring/common/config/MongoConfig.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/common/config/MongoConfig.java
@@ -4,8 +4,11 @@ import com.mongodb.ConnectionString;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
 @Configuration
@@ -22,5 +25,16 @@ public class MongoConfig {
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate(ReactiveMongoDatabaseFactory factory) {
         return new ReactiveMongoTemplate(factory);
+    }
+
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory() {
+        return new SimpleMongoClientDatabaseFactory(new ConnectionString(mongoConnectionString));
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory factory) {
+        return new MongoTemplate(factory);
     }
 }
