2a2b0a687cd46d173e9e091bc0b9c20298717475
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-04-06 22:42:04 +0200

    create mapper for objToDoc & create working pushTrackedShip function -> (todos)

--- src/main/java/pl/bartlomiej/marineunitmonitoring/common/util/MapperUtils.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/common/util/MapperUtils.java
@@ -1,8 +1,13 @@
 package pl.bartlomiej.marineunitmonitoring.common.util;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.bson.Document;
 import org.springframework.beans.BeanUtils;
 
+import static java.util.Arrays.stream;
+
+@Slf4j
 public class MapperUtils {
 
     @SneakyThrows
@@ -10,5 +15,21 @@ public class MapperUtils {
         T target = targetClass.getDeclaredConstructor().newInstance();
         BeanUtils.copyProperties(source, target);
         return target;
+    }
+
+    public static Document objectToDocument(Object object) {
+        return stream(object.getClass().getDeclaredFields())
+                .peek(field -> field.setAccessible(true))
+                .collect(
+                        Document::new,
+                        (document, field) -> {
+                            try {
+                                document.append(field.getName(), field.get(object));
+                            } catch (IllegalAccessException e) {
+                                log.error("Error from objToDoc mapping: {}", e.getMessage());
+                            }
+                        },
+                        Document::putAll
+                );
     }
 }
--- src/main/java/pl/bartlomiej/marineunitmonitoring/user/repository/CustomUserRepositoryImpl.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/user/repository/CustomUserRepositoryImpl.java
@@ -7,20 +7,22 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 import pl.bartlomiej.marineunitmonitoring.user.nested.TrackedShip;
 
+import static pl.bartlomiej.marineunitmonitoring.common.util.MapperUtils.objectToDocument;
+
 @Repository
 @RequiredArgsConstructor
 public class CustomUserRepositoryImpl implements CustomUserRepository {
 
     private final MongoTemplate mongoTemplate;
 
-    @Override // dodawanie dziala tylko jeszcze codec exception
+    @Override // todo: validation
     public TrackedShip pushTrackedShip(String id, TrackedShip trackedShip) {
         mongoTemplate.getDb().getCollection("users")
                 .updateOne(
                         new Document("_id", new ObjectId(id)),
                         new Document(
                                 "$push",
-                                new Document("trackedShips", trackedShip) //todo codecexception
+                                new Document("trackedShips", objectToDocument(trackedShip))
                         )
                 );
         return trackedShip;
