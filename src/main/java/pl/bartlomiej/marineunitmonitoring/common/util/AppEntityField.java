e3012b6cdd51744b1786328fe284fb6d04fd5104
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-05-05 17:01:47 +0200

    add x and y fields

--- src/main/java/pl/bartlomiej/marineunitmonitoring/common/util/AppEntityField.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/common/util/AppEntityField.java
@@ -4,7 +4,9 @@ public enum AppEntityField {
     SHIP_TRACK_HISTORY("ship_track_history"),
     ID("_id"),
     MMSI("mmsi"),
-    READING_TIME("readingTime");
+    READING_TIME("readingTime"),
+    X("x"),
+    Y("y");
 
     public final String fieldName;
 
