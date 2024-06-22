63c4348452b330c4768f993dc9a68198071eb7fd
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-06-23 00:03:25 +0200

    implemented granted authorities converter

--- src/main/java/pl/bartlomiej/marineunitmonitoring/security/CustomJwtGrantedAuthoritiesConverter.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/security/CustomJwtGrantedAuthoritiesConverter.java
@@ -11,8 +11,6 @@ import pl.bartlomiej.marineunitmonitoring.user.service.sync.UserService;
 
 import java.util.Collection;
 
-import static java.util.stream.Collectors.toList;
-
 @Component
 public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
@@ -26,7 +24,7 @@ public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Coll
     public Collection<GrantedAuthority> convert(Jwt source) {
         User subjectUser = userService.getUserByOpenId(source.getSubject());
         return subjectUser.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority(role.name()))
-                .collect(toList()); // why this working, but .toList() not
+                .<GrantedAuthority>map(role -> new SimpleGrantedAuthority(role.name()))
+                .toList();
     }
 }
