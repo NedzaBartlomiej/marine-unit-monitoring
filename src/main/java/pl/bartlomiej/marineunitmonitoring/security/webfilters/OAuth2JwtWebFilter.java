538c3f6107c3ae23b169b553cf791852432cfa39
Bartłomiej Nędza <bartek21122006@gmail.com>
2024-06-23 00:04:09 +0200

    implemented oauth2jwtwebfilter

--- src/main/java/pl/bartlomiej/marineunitmonitoring/security/webfilters/OAuth2JwtWebFilter.java
+++ src/main/java/pl/bartlomiej/marineunitmonitoring/security/webfilters/OAuth2JwtWebFilter.java
@@ -7,10 +7,15 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
+import pl.bartlomiej.marineunitmonitoring.user.User;
+import pl.bartlomiej.marineunitmonitoring.user.repository.reactive.ReactiveMongoUserRepository;
 import pl.bartlomiej.marineunitmonitoring.user.service.reactive.ReactiveUserService;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 
+import static java.util.List.of;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static pl.bartlomiej.marineunitmonitoring.user.nested.Role.ROLE_SIGNED;
 import static reactor.core.publisher.Mono.error;
 
 @Component
@@ -18,27 +23,41 @@ import static reactor.core.publisher.Mono.error;
 public class OAuth2JwtWebFilter implements WebFilter {
 
     private final ReactiveUserService reactiveUserService;
+    private final ReactiveMongoUserRepository reactiveMongoUserRepository;
 
-    public OAuth2JwtWebFilter(ReactiveUserService reactiveUserService) {
+    public OAuth2JwtWebFilter(ReactiveUserService reactiveUserService, ReactiveMongoUserRepository reactiveMongoUserRepository) {
         this.reactiveUserService = reactiveUserService;
+        this.reactiveMongoUserRepository = reactiveMongoUserRepository;
     }
 
-
+    @NonNull
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
 
         if (this.shouldNotFilter(exchange)) return chain.filter(exchange);
 
-        // logic
         return this.extractJwtFromRequest(exchange)
-                .map(jwt -> {
-                    log.info("Jwt received: {}", jwt.getTokenValue());
-                    return jwt;
-                })
-                .then(chain.filter(exchange));
+                .flatMap(jwt -> {
+
+                    String openId = jwt.getSubject();
+                    String email = jwt.getClaimAsString("email");
+                    String username = jwt.getClaimAsString("name");
+
+                    return reactiveUserService.getUserByOpenId(openId)
+                            .flatMap(user -> {
+                                user.setEmail(email);
+                                user.setUsername(username);
+                                return reactiveMongoUserRepository.save(user);
+                            })
+                            .switchIfEmpty(reactiveUserService.createUser(
+                                    new User(openId, username, email, of(ROLE_SIGNED)))
+                            );
+
+                }).then(chain.filter(exchange));
     }
 
     private boolean shouldNotFilter(final ServerWebExchange exchange) {
+        // shouldNotFilter when Google is not issuer
         return
                 exchange.getRequest().getHeaders().get(AUTHORIZATION) == null;
     }
@@ -48,6 +67,6 @@ public class OAuth2JwtWebFilter implements WebFilter {
                 .cast(AbstractOAuth2TokenAuthenticationToken.class)
                 .filter(authToken -> authToken.getToken() instanceof Jwt)
                 .map(authToken -> (Jwt) authToken.getToken())
-                .switchIfEmpty(error(new Throwable("Invalid token. JWT is required type."))); // handle
+                .switchIfEmpty(error(new Throwable("Invalid token. JWT is required type.")));
     }
 }
